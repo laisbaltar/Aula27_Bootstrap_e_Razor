c1e14ef512ad5a9eef1284fb853b66a7b591a227
Laís Nascimento Baltar Simões <lah.baltar@gmail.com>
2022-07-12 16:52:46 -0300

    Adicionei o que foi desenvolvido durante a mesa de trabalho

---
+++ Aula27_Bootstrap_e_Razor/Aula27_Bootstrap_e_Razor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aula27_Bootstrap_e_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
